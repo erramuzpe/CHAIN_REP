57ce25b375df67fe838e33fcb96e1de01aee4b96
erramuzpe <asier.erramuzpe@gmail.com>
2015-10-06 10:38:39 +0200

    changes in output, corrected vectors

--- EXP_SAVER/saver.py
+++ EXP_SAVER/saver.py
@@ -6,6 +6,27 @@ import pylab as pl
 
 # https://math.stackexchange.com/questions/920351/selecting-at-least-one-ball-of-each-color
 # https://en.wikipedia.org/wiki/Hypergeometric_distribution
+
+
+# Example. From the example given on the web Wikipedia: The Free 
+#  Encyclopedia [http://en.wikipedia.org/wiki/Hypergeometric_distribution].
+#  Suppose there are 5 black, 10 white, and 15 red marbles in an urn. You
+#  reach in and randomly select six marbles without replacement. What is 
+#  the probability that you pick exactly two of each color?
+#
+#              ele_set = [2,2,2]; comb = [5,10,15];
+#
+# Calling the function: 
+#             x = multivariate_hypergeometric_like(comb, ele_set)
+#
+# Answer is: (in format long)
+#
+# x =
+#
+#   0.07957559681698
+
+
+
 from pymc import multivariate_hypergeometric_like
 
 def multichoose(n,k):
@@ -33,33 +54,33 @@ p = input('Tell me the probability you want to ensure (0.95?): \n')
 x = input('Tell me the number of mutations (classes): \n')
 n = input('Tell me the number of elements in total (1000?): \n')
 
-
-num_ele = np.floor((n/x)) #number of elements x set
-ele_set  = np.ones(x)*num_ele #elements x set fixed
-
-a=0 # unknown - starts in x
-cum_prob = 0
-vector_a= []
-vector_cum_prob=[]
-
-while cum_prob<p:
-    a += 1
-    all_comb = multichoose(x,a)
-    our_comb=remove_comb(all_comb)
+for x in range(2,7):
+    num_ele = np.floor((n/x)) #number of elements x set
+    ele_set  = np.ones(x)*num_ele #elements x set fixed
+    
+    a=0 # unknown - starts in x
     cum_prob = 0
-    for comb in our_comb:
-        if not comb:
-            break
-        
-        log_like=multivariate_hypergeometric_like(comb, ele_set)
-        prob_one_comb=np.e**log_like
-        
-        cum_prob += prob_one_comb
+    vector_a= np.zeros(1)
+    vector_cum_prob= np.zeros(1)
     
+    while cum_prob<p:
+        a += 1
+        all_comb = multichoose(x,a)
+        our_comb=remove_comb(all_comb)
+        cum_prob = 0
+        for comb in our_comb:
+            if not comb:
+                break
+            
+            log_like=multivariate_hypergeometric_like(comb, ele_set)
+            prob_one_comb=np.e**log_like
+            
+            cum_prob += prob_one_comb
+        
+        print "Number of experiments:", a ,"\n" ,"Prob:", cum_prob ,"\n"
+        vector_a = np.append(vector_a,[a])
+        vector_cum_prob = np.append(vector_cum_prob, [cum_prob])
+        
     print "Number of experiments:", a ,"\n" ,"Prob:", cum_prob ,"\n"
-    vector_a = vector_a.append(a)
-    vector_cum_prob = vector_cum_prob.append(cum_prob)
-    
-print "Number of experiments:", a ,"\n" ,"Prob:", cum_prob ,"\n"
 
 pl.plot(vector_a, vector_cum_prob)
